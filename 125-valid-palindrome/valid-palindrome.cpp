ab0b466b38f94136c94fb9226d934d90a01ee627
Gaurav Jha <105377432+GauravJha610@users.noreply.github.com>
2023-12-28 22:24:47 +0530

    Time: 6 ms (44.69%) | Memory: 7.9 MB (38.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-      string str="";
-      int n=s.size();
-      for(int i=0;i<n;i++){
-          if((s[i]>=97 && s[i]<=122) || (s[i]>=48 && s[i]<=57)){
-              str+=s[i];
-          }
-          else if(s[i]>=65 && s[i]<=90){
-              str+=(s[i]+32);
-          }
-      }
-      n=str.size();
-      if(n<=1) return true;
-      int i=0,j=n-1;
-      while(i<j){
-          if(str[i]!=str[j]){
-              return false;
-          }
-          i++;j--;
-      }
+        string ans="";
+        for(char i:s){
+            if(iswalnum(i)){
+                if(isalpha(i)){
+                    ans+=tolower(i);
+                }
+                else{
+                    ans+=i;
+                }
+            }
+        }
+
+        int i=0,j=ans.size()-1;
+        while(i<j){
+            if(ans[i]!=ans[j]){
+                return false;
+            }
+            i++;j--;
+        }
+
         return true;
-    } 
+    }
 };
