92562e8e97485dddea3fc4e44de6afc55ea54bbf
Gaurav Jha <105377432+GauravJha610@users.noreply.github.com>
2023-12-29 23:19:45 +0530

    Time: 114 ms (6.04%) | Memory: 93.8 MB (29.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,30 +3,16 @@ public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         if(n<2) return 0;
-        vector<int> left(n);
-        // vector<int> maxArr(n);
-
-        left[0]=prices[0];
-        // maxArr[n-1]=prices[n-1];
-
+        int mini=prices[0];
+        int ans=0;
         for(int i=1;i<n;i++){
-            if(prices[i]<left[i-1]){
-                left[i]=prices[i];
+            if(prices[i]<mini){
+                mini=prices[i];
             }
             else{
-                left[i]=left[i-1];
-            }
-            // maxArr[j]=max(maxArr[j],maxArr[j+1]);
-        }
-
-        int ans=0;
-        for(int i=0;i<n;i++){
-            // cout<<prices[i]-left[i]<<endl;
-            if((prices[i]-left[i])>ans){
-                ans=prices[i]-left[i];
+                ans=max(ans,prices[i]-mini);
             }
         }
-
         return ans;
     }
 };
