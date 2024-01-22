490a38d43dbd2fc96fc7ed5535a73f96d80ce991
Gaurav Jha <105377432+GauravJha610@users.noreply.github.com>
2024-01-22 12:09:42 +0530

    Time: 26 ms (61.46%) | Memory: 24.8 MB (34.75%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int dup = -1, missing = -1;
-        
-        for (int i = 1; i <= nums.size(); i++) {
-            int count = 0;
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] == i) {
-                    count++;
-                }
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int mis=-1,dup=-1;
+        for(int i=0;i<n-1;i++){
+            int diff=nums[i+1]-nums[i];
+            if(diff==0 && dup==-1){
+                dup=nums[i];
             }
-            if (count == 2) {
-                dup = i;
-            } else if (count == 0) {
-                missing = i;
+            else if(diff==2 && mis==-1){
+                mis=nums[i]+1;
             }
         }
-        
-        return {dup, missing};
-        // int n=nums.size();
-        // sort(nums.begin(),nums.end());
-        // int mis=-1,dup=-1;
-        // for(int i=0;i<n-1;i++){
-        //     int diff=nums[i+1]-nums[i];
-        //     if(diff==0 && dup==-1){
-        //         dup=nums[i];
-        //     }
-        //     else if(diff==2 && mis==-1){
-        //         mis=nums[i]+1;
-        //     }
-        // }
-        // if(nums[n-1]!=n && mis==-1){
-        //     mis=n;
-        // }
-        // if(mis==-1){
-        //     mis=1;
-        // }
-        // return {dup,mis};
+        if(nums[n-1]!=n && mis==-1){
+            mis=n;
+        }
+        else if(mis==-1){
+            mis=1;
+        }
+        return {dup,mis};
     }
 };
