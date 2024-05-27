2b02f0951c3de79aad95b09b3530e1988b07fb8c
Gaurav Jha <105377432+GauravJha610@users.noreply.github.com>
2024-05-28 04:58:03 +0530

    Time: 2 ms (42.56%) | Memory: 7.9 MB (46.65%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        // if(numRows==1){
-        //     return {{1}};
-        // }
-        // if(numRows==2){
-        //     return {{1},{1,1}};
-        // }
-
         vector<vector<int>> ans(numRows);
         int count=1;
         for(int i=0;i<numRows;i++){
